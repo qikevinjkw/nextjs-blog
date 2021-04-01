71009098832caaae1d139dd968293a1ed2c0172c
kevin qi <kevinqi@kevins-MBP-2.fios-router.home>
2021-04-01 14:48:48 -0400

    flat mode works

--- components/BookStack.tsx
+++ components/BookStack.tsx
@@ -3,10 +3,12 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { animated, interpolate, useSprings } from "react-spring";
 import { useDrag } from "react-use-gesture";
 import { shuffle } from "lodash";
+import { BookDisplayMode } from "../pages/books";
+import { isBrowser } from "../lib/utils";
 
 const BOOKS: string[] = [
   "/images/atomic_habits.jpeg",
@@ -19,14 +21,34 @@ const BOOKS: string[] = [
   "/images/outliers.jpeg",
   "/images/suspect_x.jpeg",
 ];
+const BOOK_WIDTH = 500;
+const BOOK_HEIGHT = 310;
+const to = (i: number, displayMode: BookDisplayMode) => {
+  if (!isBrowser()) {
+    return {};
+  }
+  if (displayMode === "stacked") {
+    return {
+      x: window.innerWidth / 2 - BOOK_WIDTH / 3,
+      y: window.innerHeight / 6,
+      scale: 1,
+      rot: -10 + Math.random() * 20,
+      delay: i * 100,
+    };
+  } else {
+    const scale = 0.5;
+    const newWidth = BOOK_WIDTH * scale;
+    const newHeight = BOOK_HEIGHT * (scale + 0.3);
+    const booksPerRow = Math.floor(window.innerWidth / newWidth);
 
-const to = (i) => ({
-  x: 0,
-  y: i * -4,
-  scale: 1,
-  rot: -10 + Math.random() * 20,
-  delay: i * 100,
-});
+    return {
+      transformOrigin: "-50% -50%",
+      x: (i % booksPerRow) * newWidth - 50,
+      y: Math.floor(i / booksPerRow) * newHeight,
+      scale: 0.5,
+    };
+  }
+};
 const from = (i) => ({ x: 0, rot: 0, scale: 1, y: 1000 });
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r, s) =>
@@ -34,13 +56,28 @@ const trans = (r, s) =>
     r / 10
   }deg) rotateZ(${r}deg) scale(${s})`;
 
-export function BookStack() {
+export function BookStack({ displayMode }: { displayMode: BookDisplayMode }) {
   const [books, setBooks] = useState(BOOKS);
   const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
+  // @ts-ignore
   const [props, set] = useSprings(books.length, (i) => ({
-    ...to(i),
+    ...to(i, displayMode),
     from: from(i),
   })); // Create a bunch of springs using the helpers above
+
+  useEffect(() => {
+    // @ts-ignore
+    set((i: number) => {
+      // const isGone = gone.has(index);
+      // const x = isGone ? (200 + window.innerWidth) * xdir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+      // const y = isGone ? (200 + window.innerHeight) * ydir : down ? my : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+
+      // const rot = mx / 100 + (isGone ? xdir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
+      // const scale = down ? 1.1 : 1; // Active cards lift up a bit
+      return to(i, displayMode);
+    });
+  }, [displayMode]);
+
   const bind = useDrag(
     ({
       args: [index],
@@ -90,19 +127,20 @@ export function BookStack() {
           css={css`
             position: absolute;
             will-change: transform;
-            height: 500px;
-            width: 310px;
+            height: ${BOOK_HEIGHT}px;
+            width: ${BOOK_WIDTH}px;
           `}
           key={i}
           style={{
             transform: interpolate(
               [x, y],
+              // (x, y) => `translate(${x}px,${y}px,0) scale(${scale})`
               (x, y) => `translate3d(${x}px,${y}px,0)`
             ),
           }}
         >
           <animated.div
-            {...bind(i)}
+            {...(displayMode === "stacked" ? bind(i) : {})}
             style={{
               willChange: "transform",
               transform: interpolate([rot, scale], trans),
@@ -115,191 +153,3 @@ export function BookStack() {
     </>
   );
 }
-
-// const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
-// const trans = (r, s) =>
-//   `perspective(1500px) rotateX(30deg) rotateY(${
-//     r / 10
-//   }deg) rotateZ(${r}deg) scale(${s})`;
-
-// export function BookStack() {
-//   const gone = useRef(new Set()); // The set flags all the cards that are flicked out
-//   const [springs, set] = useSprings(books.length, (i) => {
-//     console.log("initial set");
-//     return {
-//       x: 0,
-//       y: i * -4,
-//       scale: 1,
-//       rot: -10 + Math.random() * 20,
-//       delay: i * 100,
-//       from: {
-//         x: 0,
-//         rot: 0,
-//         scale: 1.5,
-//         y: -500,
-//       },
-//     };
-//   }); // Create a bunch of springs using the helpers above
-//   console.log("props", springs, set);
-//   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-//   const bind = useGesture({
-//     onDrag: ({
-//       args: [index],
-//       down,
-//       delta: [xDelta, yDelta],
-//       distance,
-//       velocity,
-//       direction: [xDir],
-//     }) => {
-//       //   const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-//       const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-//       //   if (!down && trigger) {
-//       //     gone.current.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-//       //   }
-//       //   const isGone = gone.current.has(index);
-//       //   const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-//       const x = down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-//       //   const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-//       const rot = xDelta / 100; // How much the card tilts, flicking it harder makes it rotate faster
-
-//       const scale = down ? 1.1 : 1; // Active cards lift up a bit
-//       //   console.log("onDrag x=", x);
-
-//       set((i) => {
-//         if (i === index) {
-//           console.log("set ", xDelta, yDelta);
-//           return {
-//             // y: yDelta,
-//             x: xDelta,
-//             rot,
-//             scale,
-//           };
-//         }
-//       });
-//       //   [index].set({
-//       //     x,
-//       //     rot,
-//       //     scale,
-//       //   });
-//     },
-//     // onDragStart: ({
-//     //   args: [index],
-//     //   down,
-//     //   delta: [xDelta],
-//     //   distance,
-//     //   velocity,
-//     //   direction: [xDir],
-//     // }) => {
-//     //   console.log("onDrag", index, down, xDelta, distance, velocity, xDir);
-//     //   const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-//     //   const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-//     //   if (!down && trigger) {
-//     //     gone.current.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-//     //   }
-//     //   const isGone = gone.current.has(index);
-//     //   const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-//     //   const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-//     //   const scale = down ? 1.1 : 1; // Active cards lift up a bit
-//     //   springRefs.current[index].set({
-//     //     x,
-//     //     rot,
-//     //     scale,
-//     //   });
-//     // },
-//   });
-//   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-//   return (
-//     <div>
-//       {springs.map(({ x, y, rot, scale }, i) => {
-//         console.log("springs x", x);
-//         return (
-//           <animated.div
-//             key={i}
-//             style={{
-//               transform: interpolate(
-//                 [x, y],
-//                 (x, y) => `translate3d(${x}px,${y}px,0)`
-//               ),
-//               //   transform: to([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`),
-//             }}
-//             css={css`
-//               position: absolute;
-//               width: 100vw;
-//               height: 100vh;
-//               will-change: transform;
-//               display: flex;
-//               align-items: center;
-//               justify-content: center;
-//             `}
-//           >
-//             <animated.div
-//               {...bind(i)}
-//               //   style={{
-//               //     transform: interpolate([rot, scale], trans),
-//               //   }}
-//               css={css`
-//                 background-color: white;
-//                 background-size: auto 85%;
-//                 background-repeat: no-repeat;
-//                 background-position: center center;
-//                 width: 45vh;
-//                 max-width: 300px;
-//                 height: 85vh;
-//                 max-height: 570px;
-//                 will-change: transform;
-//                 border-radius: 10px;
-//                 box-shadow: 0 12.5px 100px -10px rgba(50, 50, 73, 0.4),
-//                   0 10px 10px -10px rgba(50, 50, 73, 0.3);
-//               `}
-//             >
-//               <Image
-//                 draggable={false}
-//                 src={books[i]}
-//                 alt={books[i]}
-//                 width={300}
-//                 height={500}
-//               />
-//             </animated.div>
-//           </animated.div>
-//           // <animated.div {...bind(i)} key={i}
-//           // style={{ transform: interpolate([rot, scale], trans) }}
-//           // // style={props}
-//           // >
-//           //   <Image src={books[i]} alt={books[i]} width={300} height={500} />
-//           // </animated.div>
-//         );
-//       })}
-//     </div>
-//   );
-// }
-/**
- *   ({
-        args: [index],
-        down,
-        delta: [xDelta],
-        distance,
-        direction: [xDir],
-        velocity,
-      }) => {
-        const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-        const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-        if (!down && trigger) gone.current.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-        set((i) => {
-          if (index !== i) return; // We're only interested in changing spring-data for the current spring
-          const isGone = gone.current.has(index);
-          const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-          const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-          const scale = down ? 1.1 : 1; // Active cards lift up a bit
-          return {
-            x,
-            rot,
-            scale,
-            delay: undefined,
-            config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-          };
-        });
-        if (!down && gone.current.size === books.length)
-          setTimeout(() => gone.current.clear() || set((i) => to(i)), 600);
-      }
- */
--- pages/books.tsx
+++ pages/books.tsx
@@ -2,22 +2,85 @@
 import { css, jsx } from "@emotion/react";
 import { BookStack } from "../components/BookStack";
 import Head from "next/head";
+import { useState } from "react";
+import { capitalize } from "lodash";
 
+export type BookDisplayMode = "stacked" | "flat";
 export default function Books() {
+  const [displayMode, setDisplayMode] = useState<BookDisplayMode>("stacked");
   return (
     <div
       css={css`
         width: 100%;
         height: 100%;
         display: flex;
-        justify-content: center;
-        align-items: center;
+        flex-direction: column;
       `}
     >
-      <Head>
-        <title>Favorite Books</title>
-      </Head>
-      <BookStack />
+      <div
+        css={css`
+          height: 60px;
+          margin-top: 20px;
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <button
+          css={css`
+            width: 110px;
+            height: 50px;
+            background: hsl(277deg 100% 33%);
+            border-radius: 12px;
+            border: none;
+            padding: 0;
+            cursor: pointer;
+            outline-offset: 4px;
+            outline: none;
+
+            &:hover > span {
+              transform: translateY(-4px);
+              transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+            }
+            &:active > span {
+              transform: translateY(-2px);
+              transition: transform 34ms;
+            }
+          `}
+          onClick={() => {
+            setDisplayMode((prev) => (prev === "flat" ? "stacked" : "flat"));
+          }}
+        >
+          <span
+            css={css`
+              display: block;
+              padding: 12px 12px;
+              border-radius: 12px;
+              font-size: 1.25rem;
+              background: hsl(277deg 100% 47%);
+              color: white;
+              transform: translateY(-3px);
+              will-change: transform;
+              transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+            `}
+          >
+            {capitalize(displayMode)}
+          </span>
+        </button>
+      </div>
+      <div
+        css={css`
+          width: 100%;
+          height: 100%;
+          display: flex;
+          /* justify-content: center;
+          align-items: center; */
+        `}
+      >
+        <Head>
+          <title>Favorite Books</title>
+        </Head>
+        <BookStack displayMode={displayMode} />
+      </div>
     </div>
   );
 }
