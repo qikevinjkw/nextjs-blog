116426b9d6b5df602d6bc0db8903c554f5c26ceb
kevin qi <kevinqi@kevins-MBP-2.fios-router.home>
2021-04-13 08:28:05 -0400

    Fix styling payment

--- components/PaymentWidget.tsx
+++ components/PaymentWidget.tsx
@@ -5,32 +5,26 @@ import { useState } from "react";
 
 const RoundImg = styled.img`
   border-radius: 50%;
-  position: absolute;
-  left: 0;
-  top: 0;
 `;
-const apps = ["venmo.png", "cashapp.png", "wechat.png"];
+const apps = ["french-fries.png", "venmo.png", "cashapp.png", "wechat.png"];
 
 export function PaymentWidget() {
-  const [showAll, setShowAll] = useState(false);
-  const [targetPaymentIndex, setTargetPaymentIndex] = useState(-1);
+  const [showAll, setShowAll] = useState(true);
+  const [targetPaymentIndex, setTargetPaymentIndex] = useState(1);
   return (
     <div
       css={css`
-        display: inline-block;
+        display: inherit;
         position: fixed;
         left: 20px;
         bottom: 20px;
         border-radius: 100%;
         will-change: transform;
-        /* background: transparent; */
         z-index: 99;
-        cursor: pointer;
-        height: 50px;
-        width: ${showAll ? "160px" : "50px"};
         transition: width 0.5s ease-in-out;
       `}
       onMouseLeave={() => {
+        console.log("mouse leave");
         setShowAll(false);
       }}
       onMouseEnter={(e) => {
@@ -39,12 +33,18 @@ export function PaymentWidget() {
     >
       {apps.map((app, i) => {
         return (
-          <div key={i}>
+          <div
+            key={i}
+            css={css`
+              position: relative;
+              padding: 5px;
+            `}
+          >
             <RoundImg
               css={css`
+                cursor: pointer;
                 z-index: ${i === 0 ? 99 : 2};
                 opacity: ${i === 0 ? 1 : showAll ? 1 : 0};
-                transform: ${showAll ? `translateX(${60 * i}px)` : 0};
                 will-change: transform;
                 transition: all 0.5s ease-in-out;
                 box-shadow: rgba(0, 0, 0, 0.4) 0px 4px 8px;
@@ -59,26 +59,30 @@ export function PaymentWidget() {
               width={50}
               height={50}
             />
-            <img
-              onMouseEnter={(event: any) => {
-                setTargetPaymentIndex(i);
-              }}
-              onMouseLeave={() => {
-                setTargetPaymentIndex(-1);
-              }}
-              css={css`
-                visibility: ${targetPaymentIndex === i ? "visible" : "hidden"};
-                position: absolute;
-                bottom: 50px;
-                left: ${-20 + i * 56}px;
-                opacity: ${targetPaymentIndex === i && showAll ? 1 : 0};
-                will-change: transform;
-                transition: all 0.3s ease-in-out;
-              `}
-              width={100}
-              height={100}
-              src={`/images/${app.split(".")[0]}_qrcode.jpg`}
-            />
+            {i > 0 && (
+              <img
+                onMouseEnter={(event: any) => {
+                  setTargetPaymentIndex(i);
+                }}
+                onMouseLeave={() => {
+                  setTargetPaymentIndex(-1);
+                }}
+                css={css`
+                  visibility: ${targetPaymentIndex === i
+                    ? "visible"
+                    : "hidden"};
+                  position: absolute;
+                  left: -20px;
+                  bottom: 65px;
+                  opacity: ${targetPaymentIndex === i && showAll ? 1 : 0};
+                  will-change: transform;
+                  transition: all 0.3s ease-in-out;
+                `}
+                width={100}
+                height={100}
+                src={`/images/${app.split(".")[0]}_qrcode.jpg`}
+              />
+            )}
           </div>
         );
       })}
--- posts/todo-tasks.md
+++ posts/todo-tasks.md
@@ -5,7 +5,7 @@ summary: "Stuff I still need to do for this site"
 image: "todolist.png"
 ---
 
-- flickering when hover payment icon, not closing when mouse leaves, sq app not circle, clean up code
+- center qr code
 - navbar styling is off with that horizontal scroll bar
 - Think of what to put on the main page
 - make book clickable, it floats to the middle, turns around and shows summary or your opinion on it
